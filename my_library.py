54cab57ac8d316d36126bde3edf116d17a9ba1ff
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-09-04 08:55:25 +0200

    fix unintentional error, build in intentional error

--- my_library.py
+++ my_library.py
@@ -1,14 +1,13 @@
 from skimage.filters import gaussian, threshold_otsu
-from skimage.measruements import label
+from skimage.measure import label
 
 def segment_image(image):
     # denoise the image
     blurred = gaussian(image, sigma=2)
 
     # binarize the image
-    threshold = threshold_otsu(blurred)
-    binary = blurred > threshold
-
+    binary = threshold_otsu(blurred)
+    
     # label connected components
     result = label(binary)
 
